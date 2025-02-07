53e59953273abca9df711cfb5e4ea3b740eeccb4
Asad Mujumder <asaduzzaman.anik.me@gmail.com>
2025-02-07 23:36:53 +0600

    Update env in deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
   // process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  // const env = loadEnv(mode, process.cwd());
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react(), tailwindcss()],
-    base: '/react-3d-dev-portfolio-1/', // env.VITE_APP_BASE_PATH ?? '/'
+    base: env.VITE_APP_BASE_PATH ?? '/',
     assetsInclude: ['**/*.glb'],
     server: {
       allowedHosts: ['intimate-seasnail-infinitely.ngrok-free.app'],
