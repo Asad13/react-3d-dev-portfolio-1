d2ecbaea07e7a68b82d89333c7a76fd11ed7ac2a
Asad Mujumder <asaduzzaman.anik.me@gmail.com>
2025-02-07 22:14:41 +0600

    Fix and Update for Github Workflows

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { RouterProvider, createBrowserRouter } from 'react-router';
 import { DefaultRouter } from './routers';
 
 const App = () => {
-  const router = createBrowserRouter([...DefaultRouter]);
+  const router = createBrowserRouter([...DefaultRouter], {
+    basename: import.meta.env.VITE_APP_BASE_PATH ?? '/',
+  });
 
   return <RouterProvider router={router} />;
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  // process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const env = loadEnv(mode, process.cwd());
+
   return {
     plugins: [react(), tailwindcss()],
+    base: env.VITE_APP_BASE_PATH ?? '/',
     assetsInclude: ['**/*.glb'],
     server: {
       allowedHosts: ['intimate-seasnail-infinitely.ngrok-free.app'],
